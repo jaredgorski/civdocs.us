63ddb2eae66376fe99335dbda06b1470a790010c
jaredgorski <jared.gorski@liferay.cloud>
2020-07-29 21:55:07 -0700

    Link to data repo for new document requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const IndexPage = ({data}) => (
         <strong>civdocs.us</strong> makes it easy to read the founding documents of the United States of America. This site consumes data from <a href={process.env.apiEndpoint} target="_blank"><strong>api.civdocs.us</strong></a>, an open API meant to make these documents more widely available online.
       </p>
       <p>
-        If you think a document should be added to the database, please open an issue on <a href="https://github.com/jaredgorski/api.civdocs.us" target="_blank">Github</a>.
+        If you think a document should be added to the database, please open an issue on <a href="https://github.com/jaredgorski/data.civdocs.us" target="_blank">Github</a>.
       </p>
       <ul className="index-list">
         {data.map((doc, index) => {
